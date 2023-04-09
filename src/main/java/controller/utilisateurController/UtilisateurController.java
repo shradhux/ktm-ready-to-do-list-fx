51c335a0419abc9151fa142d429d0214316e3042
shradha <shradhakallee@gmail.com>
2023-04-09 14:16:07 +0200

    Ajout Inscription

--- src/main/java/controller/utilisateurController/UtilisateurController.java
+++ src/main/java/controller/utilisateurController/UtilisateurController.java
@@ -13,20 +13,19 @@ public class UtilisateurController {
         req.setString(1, email);
         req.setString(2, mdp);
         ResultSet rep = req.executeQuery();
-        if (rep.next()){
+        if (rep.next()) {
             return new Utilisateur(rep.getInt(1), rep.getString(2), rep.getString(3), rep.getString(4), rep.getString(5), rep.getBoolean(6));
-        }
-        else{
+        } else {
             return null;
         }
     }
 
-    public static void SetMdp(Utilisateur user){
+    public static void SetMdp(Utilisateur user) {
         PreparedStatement req = null;
         try {
             req = new Bdd().getBdd().prepareStatement("Update user set mdp=? where id_user=?");
-            req.setString(1,user.getMdp());
-            req.setInt(2,user.getId());
+            req.setString(1, user.getMdp());
+            req.setInt(2, user.getId());
             req.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -42,6 +41,19 @@ public class UtilisateurController {
 
     }
 
+    public static void inscription(Utilisateur user) {
+        PreparedStatement req = null;
+        try {
+            req = new Bdd().getBdd().prepareStatement("INSERT INTO user (nom, prenom, mail, mdp) VALUES (?,?,?,?) ");
+            req.setString(1, user.getNom());
+            req.setString(2, user.getPrenom());
+            req.setString(3, user.getMdp());
+            req.setString(4, user.getEmail());
+            req.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
 
+    }
 }
--- src/main/java/graphicController/Inscription.java
+++ src/main/java/graphicController/Inscription.java
@@ -1,5 +1,31 @@
-package graphicController;
+package application;
+
+import controller.utilisateurController.UtilisateurController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import modele.utilisateur.Utilisateur;
 
 public class Inscription {
+
+    @FXML
+    private TextField mail;
+
+    @FXML
+    private TextField nom;
+
+    @FXML
+    private TextField prenom;
+    private TextField mdp;
+
+    @FXML
+    private Button valider;
+
+    @FXML
+    void valid(ActionEvent event) {
+        Utilisateur user = new UtilisateurController().inscription(nom.getText(),prenom.getText(),mail.getText(),mdp.getText());
+
+    }
 
 }
