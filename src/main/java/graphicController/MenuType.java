09287131f1d4df15bea1ff9d3f18f63c26db890c
shradha <shradhakallee@gmail.com>
2023-05-10 22:35:54 +0200

    work in progress menu type

--- src/main/java/graphicController/MenuType.java
+++ src/main/java/graphicController/MenuType.java
@@ -1,6 +1,11 @@
 package graphicController;
 
 import Classes.Liste;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import modele.bdd.Bdd;
 
@@ -11,15 +16,26 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class MenuType {
+
+    @FXML
+    private TableColumn<Liste, String> codeCouleur;
+
+    @FXML
+    private TableColumn<Liste, Integer> idType;
+
+    @FXML
+    private TableColumn<Liste, String> libelle;
+    @FXML private TableView<Liste> table;
+
     public void initialize(URL url, ResourceBundle resourceBundle) {
         table.refresh();
         PreparedStatement extraireType = null;
         try {
-            extraireType = new Bdd().getBdd().prepareStatement("SELECT * FROM liste");
+            extraireType = new Bdd().getBdd().prepareStatement("SELECT * FROM Type");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        ResultSet recupType = null;
+        ResultSet recupType= null;
         try {
             recupType = extraireType.executeQuery();
         } catch (SQLException e) {
@@ -44,10 +60,14 @@ public class MenuType {
             throw new RuntimeException(e);
         }
 
-        id.setCellValueFactory(new PropertyValueFactory<Liste,  Integer>("id_liste"));
-        nom.setCellValueFactory(new PropertyValueFactory<Liste,  String>("nom"));
-        description.setCellValueFactory(new PropertyValueFactory<Liste, String>("description"));
+        idType.setCellValueFactory(new PropertyValueFactory<Liste,  Integer>("idType"));
+        libelle.setCellValueFactory(new PropertyValueFactory<Liste,  String>("libelle"));
+        codeCouleur.setCellValueFactory(new PropertyValueFactory<Liste,  String>("code_couleur"));
         table.refresh();
         table.setItems(data);
         table.refresh();
+
+
+    }
+    public ObservableList<Liste> data= FXCollections.observableArrayList();
 }
