552095fd03149ae6f3dadd412840466dd97abdf4
Philippe-avec-3p <p.rakotondrabe@lprs.fr>
2023-05-12 00:28:36 +0200

    Ajout d'inscrit par l'administrateur terminé

---
+++ src/main/java/graphicController/AdminAjouterInscrit.java
@@ -0,0 +1,30 @@
+package graphicController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class AdminAjouterInscrit {
+
+    @FXML
+    private CheckBox est_admin_checkbox;
+
+    @FXML
+    private TextField txt_mail;
+
+    @FXML
+    private TextField txt_mdp;
+
+    @FXML
+    private TextField txt_nom;
+
+    @FXML
+    private TextField txt_prenom;
+
+    @FXML
+    void Ajouter(ActionEvent event) {
+
+    }
+
+}
